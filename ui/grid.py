e27cf58d377c5d978410f03602e91adea92ce8d0
Luke Kapeluck <kapeluck@ualberta.ca>
2024-01-15 09:43:44 -0700

    Minor changes to Grid.

--- ui/grid.py
+++ ui/grid.py
@@ -2,20 +2,22 @@ import pygame
 import UIBasics
 from copy import deepcopy
 
+
 class Cell(UIBasics.Content):
     def __init__(self, content:UIBasics.Content, bounding_box:UIBasics.BoundingBox):
         self.content = content
         self.bounding_box = bounding_box
-    
+    def draw(self, surface:pygame.Surface, bounding_box:UIBasics.BoundingBox):
+        # TODO
+        raise NotImplementedError("TODO")
+
 
 class BasicGrid(UIBasics.Content):
-    def __init__(self, cells:list[list[Cell]], bounding_box:UIBasics.BoundingBox, name:str=None):
-        self.cells = deepcopy(cells)
+    def __init__(self, bounding_box:UIBasics.BoundingBox, cells:list[list[Cell]]=None, shape:list[int]=None, name:str=None):
+        if cells is not None:
+            self.cells = deepcopy(cells)
         self.bounding_box = bounding_box
         self.name = name
-        self.on_toggle = UIBasics.Event()
-        self.on_toggle_off = UIBasics.Event()
-        self.on_toggle_on = UIBasics.Event()
         self.is_on = False
     def draw(self, surface:pygame.Surface, bounding_box:UIBasics.BoundingBox):
         # TODO
